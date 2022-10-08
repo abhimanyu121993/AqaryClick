1ab2da718de048914ac29c5756b5a75fdf63ba3a
DeepakVish00001 <deepakvish00001@gmail.com>
2022-10-08 17:03:00 +0530

    templete

---
+++ resources/views/home_layouts/header.blade.php
@@ -0,0 +1,199 @@
+
+
+    <header class="header-wrap style1">
+        <!-- <div class="header-top">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="header-top-left">
+                            <div class="close-header-top xl-none">
+                                <button type="button"><i class="las la-times"></i></button>
+                            </div>
+                            <div class="header-contact">
+                                <p><i class="ri-map-pin-fill"></i> 24th North Lane, Hill Town, New York</p>
+                            </div>
+                            <div class="header-contact">
+                                <a href="https://templates.hibootstrap.com/cdn-cgi/l/email-protection#b5ddd0d9d9daf5d0d6dac0c79bd6dad8"> <i class="ri-mail-send-line"></i> <span class="__cf_email__" data-cfemail="1d74737b725d787e72686f337e7270">[email&#160;protected]</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="header-top-right">
+                            <div class="lang_selctor  style1">
+                                <i class="ri-global-line"> </i>
+                                <select>
+                                    <option value="1">English</option>
+                                    <option value="2">French</option>
+                                    <option value="3">Arabic</option>
+                                </select>
+                            </div>
+                            <div class="header-social">
+                                <span>Follow us :</span>
+                                <ul class="social-profile style3">
+                                    <li><a target="_blank" href="https://facebook.com/"><i class="ri-facebook-fill"></i> </a></li>
+                                    <li><a target="_blank" href="https://linkedin.com/"> <i class="ri-linkedin-fill"></i> </a></li>
+                                    <li><a target="_blank" href="https://twitter.com/"> <i class="ri-twitter-fill"></i> </a></li>
+                                    <li><a target="_blank" href="https://instagram.com/"> <i class="ri-instagram-line"></i> </a></li>
+                                </ul>
+                            </div>
+                            <a href="login.html" class="link style3"> Login/Register</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 xl-none">
+                        <div class="header-search">
+                            <form action="#">
+                                <div class="form-group">
+                                    <input type="search" placeholder="Search Here ...">
+                                    <button type="submit"> <i class="ri-search-eye-line"></i> </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <div class="header-bottom">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-5 col-md-4 col-5 order-lg-1 order-md-1 order-1">
+                        <div class="logo">
+                            <a href="/">
+                                <img class="logo-light" src="/front/assets/img/logo.png" alt="Image">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-5 col-7 order-lg-2 order-md-3 order-3">
+                        <div class="main-menu-wrap style1">
+                            <div class="menu-close xl-none">
+                                <a href="javascript:void(0)"><i class="las la-times"></i></a>
+                            </div>
+                            <div id="menu" class="text-center">
+                                <ul class="main-menu ">
+                                    <li>
+                                        <a class="active" href="/">HOME</a>
+                                    </li>
+                                    <li>
+                                        <a href="/about-us">ABOUT US</a>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="/properties">PROERTIES</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="events.html">EVENT</a></li>
+                                            <li><a href="event-details.html">EVENT DETAILS</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="/contect-us">CONTACT US</a>
+                                    </li>
+                                    <li>
+                                        <button type="button" class="btn btn-primary btn-sm login-btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Login In</button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="mobile-bar-wrap">
+                            <div class="shopcart xl-none">
+                                <button type="button"> <i class="ri-shopping-cart-line"></i>
+                                    <span class="item-amt">10</span></button>
+                            </div>
+                            <div class="sidebar-menu xl-none">
+                                <i class="ri-equalizer-line"></i>
+                            </div>
+                            <div class="mobile-top-bar xl-none">
+                                <i class="ri-settings-3-line"></i>
+                            </div>
+                            <div class="mobile-menu xl-none">
+                                <a href='javascript:void(0)'><i class="ri-menu-line"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="col-lg-2 col-md-3  order-lg-3 order-md-2">
+                        <div class="header-menu-wrap">
+                            <div class="shopcart md-none">
+                                <button type="button"> <i class="ri-shopping-cart-line"></i>
+                                    <span class="item-amt">10</span></button>
+                            </div>
+                            <div class="sidebar-menu md-none">
+                                <i class="ri-equalizer-line"></i>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="cart-popup">
+        <div class="cart-popup-title">
+            <h2>Shopping Cart (3)</h2>
+            <button type="button" class="close-popup"><i class="las la-times"></i></button>
+        </div>
+        <div class="cart-popup-item-wrap">
+            <div class="cart-popup-item">
+                <div class="cart-popup-item-img">
+                    <img src="/front/assets/img/product/cart-thumb-1.jpg" alt="Image">
+                </div>
+                <div class="cart-popup-item-name">
+                    <p>Vougue Magazine</p>
+                    <div class="product-quantity">
+                        <div class="qtySelector">
+                            <span class="ri-subtract-line decreaseQty"></span>
+                            <input type="text" class="qtyValue" value="1" />
+                            <span class="las la-plus increaseQty"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart-popup-item-price">
+                    <button type="button"><i class="las la-times"></i></button>
+                    <span>$148.00</span>
+                </div>
+            </div>
+            <div class="cart-popup-item">
+                <div class="cart-popup-item-img">
+                    <img src="/front/assets/img/product/cart-thumb-2.jpg" alt="Image">
+                </div>
+                <div class="cart-popup-item-name">
+                    <p>Business Report Guideline</p>
+                    <div class="product-quantity">
+                        <div class="qtySelector">
+                            <span class="ri-subtract-line decreaseQty"></span>
+                            <input type="text" class="qtyValue" value="1" />
+                            <span class="las la-plus increaseQty"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart-popup-item-price">
+                    <button type="button"><i class="las la-times"></i></button>
+                    <span>$100.00</span>
+                </div>
+            </div>
+            <div class="cart-popup-item">
+                <div class="cart-popup-item-img">
+                    <img src="/front/assets/img/product/cart-thumb-3.jpg" alt="Image">
+                </div>
+                <div class="cart-popup-item-name">
+                    <p>Today's English</p>
+                    <div class="product-quantity">
+                        <div class="qtySelector">
+                            <span class="ri-subtract-line decreaseQty"></span>
+                            <input type="text" class="qtyValue" value="1" />
+                            <span class="las la-plus increaseQty"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart-popup-item-price">
+                    <button type="button"><i class="las la-times"></i></button>
+                    <span>$98.00</span>
+                </div>
+            </div>
+        </div>
+        <div class="cart-popup-total-wrap">
+            <div class="cart-popup-total">
+                <h5>TOTAL</h5>
+                <h5>$346.00</h5>
+            </div>
+            <a href="shop-left-sidebar.html" class="btn v4">Continue Shopping</a>
+            <a href="checkout.html" class="btn v3">Check Out</a>
+        </div>
+    </div>
+
