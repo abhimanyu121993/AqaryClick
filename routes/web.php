cea9b65639c52d7ac571895cb1211c7906c1425e
KillerHeman <sharmahcool5@gmail.com>
2022-10-08 12:12:29 +0530

    first

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // FrontEnd Routes
-Route::get('/',[LoginController::class, 'index'])->name('admin');
+Route::get('/admin',[LoginController::class, 'index'])->name('admin');
 Route::post('/login',[LoginController::class, 'store'])->name('login');
 
 
