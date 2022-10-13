5f283c06081555684c630c581f9f169bf0ef6bee
DeepakGautam420 <o6323756@gmail.com>
2022-10-13 14:38:31 +0530

    changes profile update code and solve some error

--- resources/views/admin/profile/edit_profile.blade.php
+++ resources/views/admin/profile/edit_profile.blade.php
@@ -8,17 +8,17 @@
 
                 <div class="position-relative mx-n4 mt-n4">
                     <div class="profile-wid-bg profile-setting-img">
-                        <img src="assets/images/profile-bg.jpg" class="profile-wid-img" alt="">
+                        {{-- <img src="assets/images/profile-bg.jpg" class="profile-wid-img" alt=""> --}}
                         <div class="overlay-content">
                             <div class="text-end p-3">
-                                <div class="p-0 ms-auto rounded-circle profile-photo-edit">
+                                {{-- <div class="p-0 ms-auto rounded-circle profile-photo-edit">
                                     <input id="profile-foreground-img-file-input" type="file"
                                         class="profile-foreground-img-file-input">
                                     <label for="profile-foreground-img-file-input"
                                         class="profile-photo-edit btn btn-light">
                                         <i class="ri-image-edit-line align-bottom me-1"></i> Change Cover
                                     </label>
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
@@ -30,17 +30,17 @@
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <div class="profile-user position-relative d-inline-block mx-auto  mb-4">
-                                        <img src="{{ asset('assets/images/users/avatar-1.jpg')}}"
+                                        <img src="{{ asset(Auth::user()->pic ?? 'assets/images/users/avatar-1.jpg')}}"
                                             class="rounded-circle avatar-xl img-thumbnail user-profile-image"
-                                            alt="user-profile-image">
+                                            alt="image">
                                         <div class="avatar-xs p-0 rounded-circle profile-photo-edit">
                                             <input id="profile-img-file-input" type="file"
                                                name="pic" disabled class="profile-img-file-input">
                                             <label for="profile-img-file-input"
                                                 class="profile-photo-edit avatar-xs">
-                                                <span class="avatar-title rounded-circle bg-light text-body">
+                                                {{-- <span class="avatar-title rounded-circle bg-light text-body">
                                                     <i class="ri-camera-fill"></i>
-                                                </span>
+                                                </span> --}}
                                             </label>
                                         </div>
                                     </div>
