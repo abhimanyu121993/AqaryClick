036f2db5b86cb62cdd5b08fe0eed52d83228a230
shivam kumar <shivambaranwal790505@gmail.com>
2022-11-29 15:21:38 +0530

    membership

---
+++ database/migrations/2022_11_29_082259_create_memberships_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('user_count');
+            $table->string('icon');
+            $table->string('font_color');
+            $table->string('text_color');
+            $table->string('bgcolor');
+            $table->boolean('is_ative');
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memberships');
+    }
+};
