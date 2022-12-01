88e266716e3a490f7f76d031a497afc12ec824b9
DeepakGautam420 <o6323756@gmail.com>
2022-12-01 11:10:37 +0530

    fix conflict

--- routes/web.php
+++ routes/web.php
@@ -58,25 +58,6 @@ Route::controller(HomeController::class)->group(function(){
     Route::post("/contact","contactSubmit")->name('contactus');
 });
 
-<<<<<<< Updated upstream
-=======
-// FrontEnd Routes
-// Route::get('/', function(){
-//     return view('welcome');
-// });
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> Stashed changes
 // Backend Routes
 Route::get('/admin',[LoginController::class, 'index'])->name('admin');
 Route::post('/login',[LoginController::class, 'store'])->name('login');
