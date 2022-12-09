0e279fabeeefc83a3ebd02a58b00e7e620c42b34
abhimanyu121993 <softpromani@gmail.com>
2022-12-09 21:23:57 +0530

    abh

--- app/Http/Controllers/MasterImportController.php
+++ app/Http/Controllers/MasterImportController.php
@@ -148,7 +148,8 @@ class MasterImportController extends Controller
                                         $electricity=Electricity::create($insertElectricData);
                                         if($electricity){
                                   
-                                        $contract_code= 'CC-'.$tenant->unittypeinfo->name?$tenant->unittypeinfo->name[0]:'NA'.'-'.$tenant->buildingDetails->zone_no.'-'.$tenant->buildingDetails->building_no.'-'.$tenant->unit_no.'-'.Carbon::now()->format('y');
+                                        // $contract_code= 'CC-'.$tenant->unittypeinfo->name?$tenant->unittypeinfo->name[0]:'NA'.'-'.$tenant->buildingDetails->zone_no.'-'.$tenant->buildingDetails->building_no.'-'.$tenant->unit_no.'-'.Carbon::now()->format('y');
+                                        $contract_code = 'CC-' . time() . rand(0, 99);
                                         dd($contract_code);
                                         Error::create(['url' => 'contract code', 'message' => $contract_code]);
                                             $lessor = Customer::firstOrCreate(['first_name' => $importData[37], 'email' => $importData[38]],['first_name' => $importData[37], 'email' => $importData[38]]);
