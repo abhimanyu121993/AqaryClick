cc4626a9bee622e0bd5218626631e988738a45d0
abhimanyu121993 <softpromani@gmail.com>
2022-12-16 13:40:43 +0530

    building

--- app/Exports/UnitExcel.php
+++ app/Exports/UnitExcel.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\Unit;
+use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -11,6 +12,17 @@ use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 class UnitExcel implements FromCollection,WithHeadings,WithStyles, WithEvents
 {
+    protected $user_id = '';
+    public function getUser()
+    {
+           if(Auth::user()->hasRole('Owner')){
+              $this->user_id = Auth::user()->id;
+          }
+          else
+          {
+              $this->user_id = Auth::user()->created_by;
+          }
+    }
     /**
     * @return \Illuminate\Support\Collection
     */
@@ -18,7 +30,13 @@ class UnitExcel implements FromCollection,WithHeadings,WithStyles, WithEvents
     {
        
         $d = collect();
-        $units= Unit::select('unit_ref','revenue','unit_status','building_id','unit_type','unit_no','unit_floor','unit_size','actual_rent','remark')->get()->append(['unit_type_name','unit_status_name','buildingDetails'])->makeHidden(['unit_status','unit_type']);
+        if (Auth::user()->hasRole('superadmin')) {
+            $units = Unit::select('unit_ref', 'revenue', 'unit_status', 'building_id', 'unit_type', 'unit_no', 'unit_floor', 'unit_size', 'actual_rent', 'remark')->get()->append(['unit_type_name', 'unit_status_name', 'buildingDetails'])->makeHidden(['unit_status', 'unit_type']);
+        }
+        else
+        {
+            $units = Unit::select('unit_ref', 'revenue', 'unit_status', 'building_id', 'unit_type', 'unit_no', 'unit_floor', 'unit_size', 'actual_rent', 'remark')->where('user_id',$this->user_id)->get()->append(['unit_type_name', 'unit_status_name', 'buildingDetails'])->makeHidden(['unit_status', 'unit_type']);
+        }
         foreach($units as $unit){
             $data = collect();
             $data->put('Building name', $unit->buildingDetails->name??'');
