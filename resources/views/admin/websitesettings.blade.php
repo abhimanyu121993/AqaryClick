8f40d136503df37d68a40550d69812bad579b8ad
sarvjeetkushwaha2002 <sarvjeetkushwaha2002@gmail.com>
2023-01-03 14:35:18 +0530

    frontend and backend

--- resources/views/admin/websitesettings.blade.php
+++ resources/views/admin/websitesettings.blade.php
@@ -1,9 +1,66 @@
 @extends('admin.includes.layout', ['breadcrumb_title' => 'Website - Setting'])
+<style>
+    #card-header {
+        background: #c8f4f6;
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+    }
+
+    #pop {
+        color: black !important;
+    }
+
+    #header1 {
+        background: #ecf0f3;
+        border: none !important;
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px 0px;
+    }
+
+    #h1 {
+        color: black;
+    }
+
+    #example {
+        font-size: 14px;
+    }
+
+    thead {
+        background: #c9e6e7 !important;
+    }
+
+    input,
+    select,
+    textarea,
+    #building_type {
+        border-radius: 10px !important;
+        border: none !important;
+        box-shadow: rgb(201, 212, 221) 3px 3px 6px 0px inset, rgba(211, 206, 206, 0.349) -3px -3px 6px 1px inset !important;
+    }
+
+    .dataTables_info,
+    .dataTables_paginate {
+        font-weight: bolder;
+    }
+
+    #btn-btn {
+        background: #ffffff;
+        color: black;
+        border: none;
+        border-radius: 10px !important;
+        box-shadow: rgb(201, 212, 221) 3px 3px 6px 0px, rgba(211, 206, 206, 0.349) -3px -3px 6px 1px;
+    }
+
+    #btn-btn:hover {
+        box-shadow: rgb(201, 212, 221) 3px 3px 6px 0px inset, rgba(211, 206, 206, 0.349) -3px -3px 6px 1px inset;
+    }
+</style>
 @section('title', 'Website')
 @section('main-content')
 <div class="container">
-    <div class="card">
-        <div class="card-header"> Setup Your Settings</div>
+    <div class="card" id="header1" >
+        <div class="card-header"id="card-header"> Setup Your Settings</div>
         <div class="card-body">
             <form action="{{route('admin.website-setting-update')}}" method="post" enctype="multipart/form-data">
                 @csrf
@@ -24,7 +81,7 @@
             </tbody>
             <tfoot>
                 <tr>
-                    <td colspan="100%"> <input type="submit" class="btn btn-danger" value="Update Setting"> </td>
+                    <td colspan="100%"> <input type="submit" id="btn-btn" class="btn btn-danger" value="Update Setting"> </td>
                 </tr>
             </tfoot>
            </table>
